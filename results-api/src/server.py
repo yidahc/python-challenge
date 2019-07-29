87afbe7d57e1fa297bdd288b172d78410632f546
yidahc <yidah.a.curiel@gmail.com>
2019-07-29 11:35:19 -0500

    api is now a layer and runs correctly on docker-compose, working on adding more layers

--- results-api/src/server.py
+++ results-api/src/server.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify
 from crud import nQueens
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+# this will run on local computer on http://0.0.0.0:5001/solutions
+@app.route('/solutions', methods=["GET"])
+def get_solutions():
     results = nQueens(10)
     return jsonify(results)
 
