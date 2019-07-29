c0e7f83ba40e1192148b40acebc9dcf65ed0e67b
yidahc <yidah.a.curiel@gmail.com>
2019-07-29 12:39:43 -0500

    front-end and api are succesfully linked, working on linking api to postgress instance

--- front-end/src/server.py
+++ front-end/src/server.py
@@ -1,11 +1,17 @@
+import requests, os
 from flask import Flask, render_template
 
+RESULTS_API_SERVER = os.environ['RESULTS_API_SERVER']
 app = Flask(__name__)
 
+# this will run on local computer on http://0.0.0.0:5000
 @app.route('/')
 def show_solutions():
-    Solutions = [[0, 2, 4, 1, 3], [0, 3, 1, 4, 2], [1, 3, 0, 2, 4], [1, 4, 2, 0, 3], [2, 0, 3, 1, 4], [2, 4, 1, 3, 0], [3, 0, 2, 4, 1], [3, 1, 4, 2, 0], [4, 1, 3, 0, 2], [4, 2, 0, 3, 1]] 
+    Solutions = requests.get(RESULTS_API_SERVER + "/solutions").json()  
+    # this is where the results-api is running within its container
     return render_template('show_solutions.html', solutions=Solutions)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+    
