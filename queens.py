24ed14a2bc4a05808faac2ad3bcbda67d3df81c1
yidahc <yidah.a.curiel@gmail.com>
2019-08-07 01:39:12 -0500

    wrote function to turn single list boards to full matrices

--- queens.py
+++ queens.py
@@ -21,11 +21,11 @@ def nQueens(n, board=[], results=[]):
 
 # checks for possible diagonal threats before adding new column placement to board
 def bishop_threatened (board, newCol):
+  r = len(board)
   # size of the board we are currently building
   # if test passes, this will be the index (row placement) of the new column placement
-  r = len(board)
-  # the rows (indexes) we have built so far
   rows = range(r)
+   # the rows (indexes) we have built so far
   
   for i in rows: # iterate through board rows (indexes in list)
     # compares the distance between new potential row placement (index) and row placement (index) we are currently iterating on (new row will always be higher than row we are on, so abs is not needed)
@@ -45,12 +45,39 @@ def checkBoard(board, n):
         # iterates through board and adds/subtracts row placement (index) and column placement (value) 
         # while eliminating any duplicate results with set (duplicate results are bishop threats to each other)
         == len(set(board[i]+i for i in cols)) 
+        # this one tests for minor diagonal threats (top right down to bottom left corner)
         # ex : [2,5,0,6,1,3] => board[i]+i => 2,6,2,9,5,8 => (0,2) and (2,0) are diagonal threats
-        == len(set(board[i]-i for i in cols))):
-        # ex2: [6,3,4,2,5] => board[i]-i => 6,2,2,-1,1 => (1,3) and (2,4) are diagonal threats
              # [6,3,4,2,5] => board[i]+i => 6,4,6,5,9  => (0,6) and (2,4) are diagonal threats
+        == len(set(board[i]-i for i in cols))):
+        # this one tests for major diagonal threats (top left down to bottom right corner)
+        # ex2: [6,3,4,2,5] => board[i]-i => 6,2,2,-1,1 => (1,3) and (2,4) are diagonal threats
         # once checked for diagonal/bishop threats, if len(set)==n then board is a valid and complete threat-free solution
         return True
   # if any check fails, it is not a valid solution
   return False
 
+# function to convert single list boards into a nested list matrix
+def matrixBoards (board):
+  newBoard = [] # creating matrix
+  queens = len(board) # amount of queens == N
+  S = range(queens)  # size of matrix (NxN)
+
+  for queen in board: # looping through board (first iteration over N)
+    # each queen value stands for its placement in its row (its column index on its new row)
+    newRow = []
+    # creating a new newRow for each queen in board (creating N rows)
+    for c in S:
+    # second iteration over N to create N columns in each row
+      if c != queen:
+      # if the column we are buidling is not where a queen should be
+        newRow.append(0)
+        # build column by adding a 0 (empty spot) to row we are building
+      else:
+      # if the column we are creating is where this row's queen placement is 
+        newRow.append(1)
+        # place a queen there instead (represented by 1)
+    newBoard.append(newRow)    
+    # adding each new row to matrix
+  return newBoard
+
+print (matrixBoards([1,3,0,2]))
